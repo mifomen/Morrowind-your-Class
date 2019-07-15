716aef4df9ee9e107361d2be86a8eb2a6e288aef
Ilya Koshelev <mifomen@yandex.ru>
2019-07-15 17:56:59 +0300

    start

---
+++ gulp/tasks/serve.js
@@ -0,0 +1,21 @@
+module.exports = function () {
+  $.gulp.task('serve', function (){
+    $.bs.init({
+      server: {
+        baseDir: 'build'
+      },
+      notify: false,
+      open: true,
+      ui: false
+    }),
+    $.gulp.watch("src/scss/**/*.scss",  $.gulp.parallel('scss'));
+    $.gulp.watch("src/pug/**/*.pug",  $.gulp.parallel('pug'));
+    $.gulp.watch("src/**/index.pug",  $.gulp.parallel('pug'));
+    $.gulp.watch("src/html/**/*.html",  $.gulp.parallel('pug'));
+    $.gulp.watch("src/js/**/*.js",  $.gulp.parallel('script'));
+    // $.gulp.watch("src/pug/html2pug/*.html",  $.gulp.series('html2pug','pug'));
+     $.gulp.watch("src/img/**/*.{png,jpg,svg}",  $.gulp.parallel('img'));
+    // $.gulp.watch("src/pug/html2pug/*.html",  $.gulp.series('html2pug','pug'));
+      // $.gulp.watch("src/pug/*.html",  $.gulp.parallel('html'));
+  })
+}
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+var links = document.getElementsByTagName('a');
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click',function (evt) {
+    evt.preventDefault();
+  })
+};
+console.log("work")
